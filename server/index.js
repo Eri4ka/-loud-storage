4d980e3f5d3a218fa2fad5c029f202d2c19b3a81
Eri4ka <navyhasanov@gmail.com>
2023-03-19 23:27:34 +0500

    init project

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const authRouter = require("./routes/auth.routes");
+
+const app = express();
+const PORT = config.get('serverPort');
+const dbUrl = config.get('dbUrl')
+
+app.use(express.json());
+app.use("/api/auth", authRouter);
+
+const start = async () => {
+  try { 
+    await mongoose.connect(dbUrl);
+
+    app.listen(PORT, () => {
+      console.log('Server started on port', PORT);
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start();
